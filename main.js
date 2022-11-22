cf02030b0415c78929501bf56c9a3ca75be7b82d
Gabriel Santos <gabi.santos.pu@gmail.com>
2022-11-22 20:57:30 -0300

    Update to code in JS

---
+++ main.js
@@ -0,0 +1,23 @@
+const caixaTxt = document.querySelectorAll('.texto')
+const imgIcone = document.querySelectorAll('.setinha')
+const paragrafoTxt = document.querySelectorAll('.paragrafo')
+const tituloTxt = document.querySelectorAll('.questao')
+
+caixaTxt.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        let valor = index
+        let todos = [0, 1, 2, 3, 4]
+        todos.forEach(elemento => {
+            if (valor != elemento) {
+                imgIcone[elemento].classList.remove('rotacao')
+                paragrafoTxt[elemento].classList.remove('mosrar-txt')
+                tituloTxt[elemento].classList.remove('tutilo-dois')
+
+            } else {
+                imgIcone[valor].classList.add('rotacao')
+                paragrafoTxt[valor].classList.add('mostrar-txt')
+                tituloTxt[valor].classList.add('titulo-dois')
+            }
+        })
+    })
+})
